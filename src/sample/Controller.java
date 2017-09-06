1ec1b16b0e5b67fd0ad9d126d935d6dab111b61b
LunziQwQ <me@lunzi.space>
2017-09-06 11:05:49 +0800

    将学习任务使用多线程实现，防止了UI变成卡死

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -11,12 +12,12 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class Controller {
 	private FileType fileType = new FileType();
-	private MachineLearning ml = new MachineLearning();
 	
 	@FXML
 	private TextArea console;
@@ -41,43 +42,53 @@ public class Controller {
 	
 	@FXML
 	void learnThePath() {
-		ml.learnCount = 0;
-		fileCount = 0;
-		console.setText("");
-		String path = filePath.getText();
-		File temp = new File(path);
-		if (!temp.exists()) {
-			console.setText("该路径不存在，请检查是否输入正确");
-		} else if (!temp.isDirectory()) {
-			console.appendText(ml.learnFile(temp));
-		} else {
-			dfsLearn(temp);
-			console.appendText("\n========================\n" +
-					"扫描共计" + fileCount + "个文件\n" +
-					"成功学习共计" + ml.learnCount + "种文件特征");
-		}
-		try {
-			fileType.saveFileTypeMap();
-			fileType.loadFileTypeMap();
-		} catch (IOException ioe) {
-			console.appendText("\n文件类型数据存储失败");
-			ioe.printStackTrace();
-		}
+		new Thread() {
+			private MachineLearning ml = new MachineLearning();
+			
+			@Override
+			public void run() {
+				ml.learnCount = 0;
+				fileCount = 0;
+				Platform.runLater(() -> console.setText(""));
+				String path = filePath.getText();
+				File temp = new File(path);
+				if (!temp.exists()) {
+					Platform.runLater(() -> console.setText("该路径不存在，请检查是否输入正确"));
+				} else if (!temp.isDirectory()) {
+					Platform.runLater(() -> console.appendText(ml.learnFile(temp)));
+				} else {
+					dfsLearn(temp);
+					Platform.runLater(() -> console.appendText("\n========================\n" +
+							"扫描共计" + fileCount + "个文件\n" +
+							"成功学习共计" + ml.learnCount + "种文件特征"));
+				}
+				try {
+					fileType.saveFileTypeMap();
+					fileType.loadFileTypeMap();
+				} catch (IOException ioe) {
+					console.appendText("\n文件类型数据存储失败");
+					ioe.printStackTrace();
+				}
+			}
+			
+			private List<File> dfsLearn(File file) {
+				List<File> fileList = new ArrayList<>();
+				
+				if (file.isDirectory()) {
+					Arrays.stream(file.listFiles()).map(this::dfsLearn).forEach(fileList::addAll);
+				} else {
+					fileCount++;
+					Platform.runLater(() -> console.appendText(ml.learnFile(file)));
+					try{
+						Thread.sleep(5);
+					}catch (Exception e){}
+				}
+				return fileList;
+			}
+		}.start();
 	}
 	
-	private List<File> dfsLearn(File file) {
-		List<File> fileList = new ArrayList<>();
-		
-		if (file.isDirectory()) {
-			for (File item : file.listFiles()) {
-				fileList.addAll(dfsLearn(item));
-			}
-		} else {
-			fileCount++;
-			console.appendText(ml.learnFile(file));
-		}
-		return fileList;
-	}
+	
 	
 	@FXML
 	private void printMap() {
